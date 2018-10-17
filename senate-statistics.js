bb7f44639244d3d0f0e8628920b7d8586a545a74
Cindy Knottnerus <cknottne@gmail.com>
2018-10-17 19:05:31 +0200

    finalized the statistics

--- senate-statistics.js
+++ senate-statistics.js
@@ -1,5 +1,10 @@
 //Global Variables
 var members = data.results[0].members;
+var sortedArray = sortArrayBottomTen();
+var sortedArrayForTop = sortArrayTopTen();
+var leastEngaged = missedVotesSortArrayTopTen();
+var mostEngaged = missedVotesSortArrayTopTen();
+
 var statistics = {
 	"numberDemocrat": stateNumber("D"),
 	"numberRepublican": stateNumber("R"),
@@ -11,25 +16,33 @@ var statistics = {
 	"averageIndependent": Math.round(arraySum("I") / stateNumber("I")),
 
 	//Least loyal (Bottom 10% of the party) - Name	Number Party Votes	% Party Votes
-	"noVoteDemocrat": 0,
-	"noVoteRepublican": 0,
-	"noVoteIndependent": 0,
+	"nameLeastLoyal": theNames(sortedArray),
+	"noPartyVLeastLoyal": arrayOfVotes(sortedArray),
+	"noPercPartyVLeastLoyal": percentageOfVotes(sortedArray),
 
 	//Most loyal (Top 10% of the party) - Name	Number Party Votes	% Party Votes
-	"voteDemocrat": 0,
-	"voteRepublican": 0,
-	"voteIndependent": 0,
+	"nameMostLoyal": theNames(sortedArrayForTop),
+	"noPartyVMostLoyal": arrayOfVotes(sortedArrayForTop),
+	"noPercPartyVMostLoyal": percentageOfVotes(sortedArrayForTop),
 
 	//Least Engaged (Bottom 10% of the party) - Name Number of Missed Votes	% Missed
-	"missedMostVotesDemocrat": 0,
-	"missedMostVotesRepublican": 0,
-	"missedMostVoteIndependent": 0,
+	"nameLeastEngaged": theNames(leastEngaged),
+	"noMissedVotesLeastEngaged": arrayOfMissedVotes(leastEngaged),
+	"PercMissedVotesLeastEngaged": percentageOfMissedVotes(leastEngaged),
 
 	//Most Engated (Top 10% of the party ) - Name Number of Missed Votes % Missed
-	"missedLeastVotesDemocrat": 0,
-	"missedLeastVotesRepublican": 0,
-	"missedLeastVoteIndependent": 0
+	"nameMostEngaged": theNames(mostEngaged),
+	"noMissedVotesMostEngaged": arrayOfMissedVotes(mostEngaged),
+	"PercMissedVotesMostEngaged": percentageOfMissedVotes(mostEngaged),
 };
+
+createTable(leastEngaged)
+
+
+
+
+
+
 
 function stateNumber(party) {
 	var array = [];
@@ -52,33 +65,140 @@ function arraySum(party) {
 		if (members[i].party == party) {
 			total += members[i].votes_with_party_pct;
 		}
-
-
 	}
-	return total;
-}
-sortArray();
 
-function sortArray() {
+	return (total);
+}
+
+///////////////////////////////////////////////////////
+
+function sortArrayBottomTen() {
 	members.sort(function (a, b) {
 		return a.votes_with_party_pct - b.votes_with_party_pct;
 	});
 	var x = tenPrcent(members)
-	return members;
+	return x;
+}
+
+function missedVotesSortArrayBottomten() {
+	members.sort(function (a, b) {
+		return a.missed_votes_pct - b.missed_votes_pct;
+	});
+	var x = tenPrcent(members)
+	return x;
+}
+
+function sortArrayTopTen() {
+	members.sort(function (a, b) {
+		return b.votes_with_party_pct - a.votes_with_party_pct;
+	});
+	var y = tenPrcent(members)
+	return y;
+}
+
+function missedVotesSortArrayTopTen() {
+	members.sort(function (a, b) {
+		return b.missed_votes_pct - a.missed_votes_pct;
+	});
+	var x = tenPrcent(members)
+	return x;
 }
 
 function tenPrcent(array) {
-	
+	var newArray = [];
+
+	for (var i = 0; i < array.length; i++) {
+		if (i < (array.length * 0.1)) {
+			newArray.push(array[i])
+		} else if (array[i] == array[i - 1]) {
+			newArray.push(array[i])
+		} else {
+			break;
+		}
+	}
+	return newArray
 }
 
+function arrayOfVotes(array) {
+	var empty = [];
+	for (var i = 0; i < array.length; i++) {
+		empty.push(array[i].total_votes)
 
-//function arraySum(party) {
-//	var total = 0;
-//
-//		for (var i = 0; i < members.length; i++) {
-//			if (members[i].party == party) {
-//				total += members[i].votes_with_party_pct;
-//			}
-//
-//		} return total;
-//}
+	}
+	return empty
+}
+
+function arrayOfMissedVotes(array) {
+	var empty = [];
+	for (var i = 0; i < array.length; i++) {
+		empty.push(array[i].missed_votes)
+
+	}
+	return empty
+}
+
+function percentageOfVotes(array) {
+	var empty = [];
+	for (var i = 0; i < array.length; i++) {
+		empty.push(array[i].votes_with_party_pct)
+
+	}
+	return empty
+}
+
+function percentageOfMissedVotes(array) {
+	var empty = [];
+	for (var i = 0; i < array.length; i++) {
+		empty.push(array[i].missed_votes_pct)
+
+	}
+	return empty
+}
+
+function theNames(array) {
+	var empty = [];
+	for (var i = 0; i < array.length; i++) {
+		if (array[i].middle_name == null) {
+			empty.push(array[i].first_name + " " + array[i].last_name)
+		} else {
+			empty.push(array[i].first_name + " " + array[i].middle_name + " " + array[i].last_name);
+		}
+	}
+	return empty
+}
+
+function createTable(array) {
+
+	var table = document.getElementById("politics-data");
+	var tbdy = document.getElementById("people");
+	tbdy.innerHTML = "";
+
+	for (var i = 0; i < array.length; i++) {
+		var row = document.createElement("TR");
+		var name;
+		if (array[i].middle_name == null) {
+			name = array[i].first_name + " " + array[i].last_name
+		} else {
+			name = array[i].first_name + " " + array[i].middle_name + " " + array[i].last_name;
+		}
+
+		var missedVotesPct = array[i].missed_votes_pct;
+		var missedVotes = array[i].missed_votes;
+
+		var nameCell = document.createElement("TD");
+		nameCell.innerHTML = name;
+		row.appendChild(nameCell);
+		tbdy.appendChild(row);
+		
+		var nameCell = document.createElement("TD");
+		nameCell.innerHTML = missedVotes;
+		row.appendChild(nameCell);
+		tbdy.appendChild(row);
+		
+		var nameCell = document.createElement("TD");
+		nameCell.innerHTML = missedVotesPct;
+		row.appendChild(nameCell);
+		tbdy.appendChild(row);
+
+	}
+}
