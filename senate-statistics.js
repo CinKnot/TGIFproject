b76da857f88ac1addee0f1446d44e9c5b0255a2d
Cindy Knottnerus <cknottne@gmail.com>
2018-10-18 19:14:21 +0200

    tables displayed

--- senate-statistics.js
+++ senate-statistics.js
@@ -3,7 +3,9 @@ var members = data.results[0].members;
 var sortedArray = sortArrayBottomTen();
 var sortedArrayForTop = sortArrayTopTen();
 var leastEngaged = missedVotesSortArrayTopTen();
-var mostEngaged = missedVotesSortArrayTopTen();
+var mostEngaged = missedVotesSortArrayBottomten();
+var leastLoyal = sortArrayBottomTen();
+var mostLoyal = sortArrayTopTen();
 
 var statistics = {
 	"numberDemocrat": stateNumber("D"),
@@ -36,12 +38,15 @@ var statistics = {
 	"PercMissedVotesMostEngaged": percentageOfMissedVotes(mostEngaged),
 };
 
-createTable(leastEngaged)
-
-
-
-
-
+if (location.pathname == "/C:/Users/cknot/Documents/ExerciseTGIF/senate-attendance.html" || "/C:/Users/cknot/Documents/ExerciseTGIF/house-attendance.html") {
+	createTable(leastEngaged, "people")
+	createTable(mostEngaged, "mostEngaged")
+	senateAtAGlanceTable();
+} else if (location.pathname == "/C:/Users/cknot/Documents/ExerciseTGIF/senate_loyalty.html") {
+	createTableLoyalty(leastLoyal, "people")
+	createTableLoyalty(mostLoyal, "mostEngaged")
+	senateAtAGlanceTable();
+}
 
 
 function stateNumber(party) {
@@ -77,6 +82,7 @@ function sortArrayBottomTen() {
 		return a.votes_with_party_pct - b.votes_with_party_pct;
 	});
 	var x = tenPrcent(members)
+	console.log(x)
 	return x;
 }
 
@@ -93,6 +99,7 @@ function sortArrayTopTen() {
 		return b.votes_with_party_pct - a.votes_with_party_pct;
 	});
 	var y = tenPrcent(members)
+	console.log(y)
 	return y;
 }
 
@@ -167,10 +174,56 @@ function theNames(array) {
 	return empty
 }
 
-function createTable(array) {
 
-	var table = document.getElementById("politics-data");
-	var tbdy = document.getElementById("people");
+
+function senateAtAGlanceTable() {
+	var table = document.getElementById("party-data");
+	var tbdy = document.getElementById("boys");
+	tbdy.innerHTML = "";
+
+	var demRow = document.createElement("TR");
+	var dataDemName = document.createElement("TD");
+	dataDemName.innerHTML = "Democrat";
+	var dataTotalDem = document.createElement("TD");
+	dataTotalDem.innerHTML = statistics["numberDemocrat"];
+	var aveDem = document.createElement("TD");
+	aveDem.innerHTML = statistics["averageDemocrat"];
+	demRow.appendChild(dataDemName);
+	demRow.appendChild(dataTotalDem);
+	demRow.appendChild(aveDem);
+	tbdy.appendChild(demRow);
+
+	var demRow = document.createElement("TR");
+	var dataDemName = document.createElement("TD");
+	dataDemName.innerHTML = "Republican";
+	var dataTotalDem = document.createElement("TD");
+	dataTotalDem.innerHTML = statistics["numberRepublican"];
+	var aveDem = document.createElement("TD");
+	aveDem.innerHTML = statistics["averageDemocrat"];
+	demRow.appendChild(dataDemName);
+	demRow.appendChild(dataTotalDem);
+	demRow.appendChild(aveDem);
+	tbdy.appendChild(demRow);
+
+	var demRow = document.createElement("TR");
+	var dataDemName = document.createElement("TD");
+	dataDemName.innerHTML = "Independent";
+	var dataTotalDem = document.createElement("TD");
+	dataTotalDem.innerHTML = statistics["numberIndependent"];
+	var aveDem = document.createElement("TD");
+	aveDem.innerHTML = statistics["averageIndependent"];
+	demRow.appendChild(dataDemName);
+	demRow.appendChild(dataTotalDem);
+	demRow.appendChild(aveDem);
+	tbdy.appendChild(demRow);
+}
+
+
+
+
+function createTable(array, id) {
+
+	var tbdy = document.getElementById(id);
 	tbdy.innerHTML = "";
 
 	for (var i = 0; i < array.length; i++) {
@@ -189,14 +242,49 @@ function createTable(array) {
 		nameCell.innerHTML = name;
 		row.appendChild(nameCell);
 		tbdy.appendChild(row);
-		
+
 		var nameCell = document.createElement("TD");
 		nameCell.innerHTML = missedVotes;
 		row.appendChild(nameCell);
 		tbdy.appendChild(row);
-		
+
 		var nameCell = document.createElement("TD");
 		nameCell.innerHTML = missedVotesPct;
+		row.appendChild(nameCell);
+		tbdy.appendChild(row);
+
+	}
+}
+
+function createTableLoyalty(array, id) {
+
+	var tbdy = document.getElementById(id);
+	tbdy.innerHTML = "";
+
+	for (var i = 0; i < array.length; i++) {
+		var row = document.createElement("TR");
+		var name;
+		if (array[i].middle_name == null) {
+			name = array[i].first_name + " " + array[i].last_name
+		} else {
+			name = array[i].first_name + " " + array[i].middle_name + " " + array[i].last_name;
+		}
+
+		var percentageOfVotes = array[i].votes_with_party_pct;
+		var totalVotes = array[i].total_votes;
+
+		var nameCell = document.createElement("TD");
+		nameCell.innerHTML = name;
+		row.appendChild(nameCell);
+		tbdy.appendChild(row);
+
+		var nameCell = document.createElement("TD");
+		nameCell.innerHTML = totalVotes;
+		row.appendChild(nameCell);
+		tbdy.appendChild(row);
+
+		var nameCell = document.createElement("TD");
+		nameCell.innerHTML = percentageOfVotes;
 		row.appendChild(nameCell);
 		tbdy.appendChild(row);
 
